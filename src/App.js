b39ee927958314c74970b7e45fd514d414216b88
Riswan Gani Padilah <riswangani11@gmail.com>
2022-02-17 05:24:17 +0700

    footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import React from 'react';
+
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from './containers';
+
+import { CTA, Brand, Navbar } from './components';
+
+import './App.css';
+
 function App() {
   return (
     <div className="App">
-     <h1>Hello World</h1>
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
